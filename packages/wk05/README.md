0319872b4095b687552058b4fac6fcb4aa417510
Jimmy Chu <jimmychu0807@gmail.com>
2023-12-17 20:05:53 +0700

    chore: added wk05 skeleton

---
+++ packages/wk05/README.md
@@ -0,0 +1,8 @@
+# Week 05
+
+- source: https://www.coursera.org/learn/crypto/quiz/7Cjo5/week-5-programming-assignment-optional
+- screenshot 01
+    ![01](assets/01.png)
+
+- screenshot 02
+    ![02](assets/02.png)
---
+++ packages/wk05/src/lib.ts
@@ -0,0 +1,11 @@
+// Given p, g, h, find x in the h = g^x in Zp*
+const DiscreteLogSolver = {
+  solve: function (p: bigint, g: bigint, h: bigint): bigint {
+    p;
+    g;
+    h;
+    return 1n;
+  },
+};
+
+export default DiscreteLogSolver;
