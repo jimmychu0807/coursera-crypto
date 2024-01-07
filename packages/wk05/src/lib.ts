33deffe558741c7c146ff66bdfcf431abd1dfcce
Jimmy Chu <jimmychu0807@gmail.com>
2024-01-07 08:24:16 +0800

    feat(wk05): wip, updated work

--- packages/wk05/src/lib.ts
+++ packages/wk05/src/lib.ts
@@ -1,11 +1,32 @@
 // Given p, g, h, find x in the h = g^x in Zp*
-const DiscreteLogSolver = {
-  solve: function (p: bigint, g: bigint, h: bigint): bigint {
+
+// default trial limit
+const TRIAL_LMT = BigInt(2) ** BigInt(20);
+
+class DiscreteLogSolver {
+  protected limit: bigint;
+
+  constructor(limit?: bigint) {
+    this.limit = limit || TRIAL_LMT;
+  }
+
+  public solve(p: bigint, g: bigint, h: bigint): bigint {
+    // building a hash table
+    const leftSideMap = this.buildMapping((input: bigint) => h / g ** input);
+
     p;
-    g;
-    h;
+    leftSideMap;
     return 1n;
-  },
-};
+  }
+
+  protected buildMapping(fn: (input: bigint) => bigint): Map<bigint, bigint> {
+    const map = new Map();
+    for (let i = BigInt(0); i < BigInt(this.limit); i++) {
+      map.set(i, fn(i));
+    }
+
+    return map;
+  }
+}
 
 export default DiscreteLogSolver;
