af33c8181fbfa63ba35ee608e153b4d4b57cd9c4
Jimmy Chu <jimmychu0807@gmail.com>
2024-02-07 15:24:24 +0800

    Completed wk06 first 3 questions (#17)
    * feat(wk06): wip
    
    * feat(wk06): solve part 3

---
+++ packages/wk06/src/lib.ts
@@ -0,0 +1,35 @@
+const DEBUG_CALC = false;
+
+interface Options {
+  ceil: boolean;
+}
+
+const defaultOpt: Options = {
+  ceil: true,
+};
+
+const NthRootCalc = {
+  // This is the referenced impl:
+  // https://gist.github.com/JochemKuijpers/cd1ad9ec23d6d90959c549de5892d6cb
+  calc: function (p: bigint, n: bigint, opt: Options = defaultOpt): bigint {
+    let min = 1n;
+    let max = p / n;
+    while (max >= min) {
+      const mid = (min + max) >> 1n;
+      DEBUG_CALC && console.log(`min: ${min}, mid: ${mid}, max: ${max}`);
+
+      const q = mid ** n;
+      if (q > p) {
+        max = mid - 1n;
+      } else {
+        min = mid + 1n;
+      }
+    }
+    const sol = min - 1n;
+
+    if (!opt.ceil) return sol;
+    return sol ** n < p ? sol + 1n : sol;
+  },
+};
+
+export { NthRootCalc as default };
